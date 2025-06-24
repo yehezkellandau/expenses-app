2f2475a347be8fa173b5ad2a71f95654577ecdac
yeheland <yeland_97@yhaoo.fr>
2025-06-24 08:37:13 -0400

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Expenses App</h1>
-      </div>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Expenses App</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">List</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+export default Header;
