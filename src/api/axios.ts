75f23501895e4422f9e35871b8071db43a8043cf
yeheland <yeland_97@yhaoo.fr>
2025-09-14 21:33:26 -0400

    update local and prod environment

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
  
 const api = axios.create({
-  baseURL: 'https://www.expenses-api.yehezkellandau.com/api/', // Replace with your real URL
+  // baseURL: 'https://www.expenses-api.yehezkellandau.com/api/', // Replace with your real URL
+  baseURL : import.meta.env.VITE_API_URL,
   withCredentials: true, // if you use cookies/session
 });
 
