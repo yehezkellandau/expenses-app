2a374cf7eb174c6a3c82d799de5148339f158833
yeheland <yeland_97@yhaoo.fr>
2025-09-18 22:50:47 -0400

    test

--- src/api/expenses.ts
+++ src/api/expenses.ts
@@ -1,15 +1,30 @@
 import api from './axios';
 import { type Expense } from '@/types/Expense';
 
-export const getExpenses = async (month: number, year: number): Promise<Expense[]> => {
-  const response = await api.get(`expenses?household_id=1&month=${month}&year=${year}`);
+// Fetch all expenses
+export const getExpenses = async (month: number, year: number, token: string): Promise<Expense[]> => {
+  const response = await api.get(`expenses?month=${month}&year=${year}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data.data;
 };
 
-export const deleteExpense = async (id: number) => {
-  await api.delete(`/expenses/${id}`);
+// Delete an expense
+export const deleteExpense = async (id: number, token: string) => {
+  await api.delete(`/expenses/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
 
-export const updateExpense = async (id: number, data: Partial<Expense>) => {
-  await api.put(`/expenses/${id}`, data);
+// Update an expense
+export const updateExpense = async (id: number, data: Partial<Expense>, token: string) => {
+  await api.put(`/expenses/${id}`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
